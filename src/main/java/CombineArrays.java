6c937c45a6ec374e8fb8f78954a35f5753de0111
Saumya Puthenveettil <sputhen2000@gmail.com>
2024-09-29 18:28:13 +0000

    Completed Coding Challenge: Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -7,8 +7,22 @@ public class CombineArrays {
      * @param arr2 an array of ints.
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
+    public static void main(String[] args){}
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int r = 2*arr1.length;
+        int[] myArray = new int[r];
+   
+        for (int i=0; i<arr1.length;i++){
+            myArray[i] = arr1[i];
+           
+        }
+    
+        for (int i=3; i<r;i++){
+             myArray[i] = arr2[i-3];
+          
+            }
+        
+        
+        return myArray;
     }
 }
